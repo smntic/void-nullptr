73fbd1c8fc8fa1890392ae0b2b1490d686ee7ae6
MrPersonDev <simonashton37@gmail.com>
2024-04-28 14:35:14 -0700

    Ensure xorg.conf.d exists

--- xorg_conf.sh
+++ xorg_conf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir /etc/X11/xorg.conf.d
+
 cp config/xorg/00-keyboard.conf /etc/X11/xorg.conf.d/
 cp config/xorg/30-touchpad.conf /etc/X11/xorg.conf.d/
 cp config/xorg/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/
