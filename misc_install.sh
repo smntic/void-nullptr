769bc08ce26058f5a62bd24f42b7c749cd88e662
MrPersonDev <simonashton37@gmail.com>
2024-04-28 17:49:19 -0700

    Add Android SDK

--- misc_install.sh
+++ misc_install.sh
@@ -20,3 +20,25 @@ rm -rf HueMaster
 # Add Flathub repository
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
+# ===== Android SDK =====
+
+# Update Java symlinks
+sudo xbps-alternatives -g jdk -s openjdk17
+
+# SDK root
+export ANDROID_SDK_ROOT=/usr/lib/android-sdk
+mkdir -p $ANDROID_SDK_ROOT
+pushd $ANDROID_SDK_ROOT
+
+# Download
+wget https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
+unzip commandlinetools-linux-11076708_latest.zip
+
+# Move folders
+mv cmdline-tools tools
+mkdir cmdline-tools
+mv tools cmdline-tools/tools
+
+# Accept licenses (don't tell them!)
+yes | ./sdkmanager --licenses
+
