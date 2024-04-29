e6b28dcc19cbcb771336bb161cfa02f5e6dc4aac
MrPersonDev <simonashton37@gmail.com>
2024-04-28 17:42:44 -0700

    Add miniconda

--- user_install.sh
+++ user_install.sh
@@ -17,3 +17,9 @@ wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.12/O
 appim -i Obsidian-1.5.12.AppImage
 rm Obsidian-1.5.12.AppImage
 
+# ===== Miniconda =====
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+Miniconda3-latest-Linux-x86_64.sh -b -p "$HOME"/bin/miniconda
+rm Miniconda3-latest-Linux-x86_64.sh
+
