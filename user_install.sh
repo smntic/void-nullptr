e3026216653a661d29afde644428d2ce272c6fbf
MrPersonDev <simonashton37@gmail.com>
2024-04-28 19:37:28 -0700

    Fix user_install.sh

--- user_install.sh
+++ user_install.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
 # ===== cp-tool =====
-
-# Clone repo
 git clone https://github.com/smnast/cp-tool
-
-# Link (please never do this)
 ln -s cp-tool/cpt "$HOME"/bin/
 ln -s cp-tool/cp-tool.py "$HOME"/bin/
-
-# Clean up
 rm -rf cp-tool
 
 # ===== Obsidian =====
 wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.12/Obsidian-1.5.12.AppImage
-appim -i Obsidian-1.5.12.AppImage
+"$HOME"/bin/appim -i Obsidian-1.5.12.AppImage
 rm Obsidian-1.5.12.AppImage
 
 # ===== Miniconda =====
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
-Miniconda3-latest-Linux-x86_64.sh -b -p "$HOME"/bin/miniconda
+./Miniconda3-latest-Linux-x86_64.sh -b -p "$HOME"/bin/miniconda
 rm Miniconda3-latest-Linux-x86_64.sh
 
