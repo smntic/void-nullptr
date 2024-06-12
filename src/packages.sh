c2b31742702f92970a87d41fa05b8dfd37239546
Simon Ashton <simonashton.dev@gmail.com>
2024-06-11 18:47:44 -0700

    Add ccache and ninja packages

--- src/packages.sh
+++ src/packages.sh
@@ -21,7 +21,8 @@ xbps-install -y ranger atool
 # Development tools
 xbps-install -y base-devel
 xbps-install -y git python3-virtualenv ffmpeg flex ncurses-devel
-xbps-install -y make cmake gcc Bear gdb
+xbps-install -y make cmake gcc Bear gdb ccache
+xbps-install -y ninja
 xbps-install -y scons pkgconf
 xbps-install -y libopencv-devel speech-dispatcher ntfs-3g
 xbps-install -y openjdk8 openjdk17
